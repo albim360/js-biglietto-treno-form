be300622850b0143b27f647b88105ee0f69deb77
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-01 17:43:11 +0100

    FEAT add output in js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,51 @@
-// Prezzo al km fisso
 const prezzoAlKm = 0.21;
 
-// Prezzo finale display element
 const prezzoFinaleDisplay = document.getElementById("prezzoFinaleDisplay");
+const nomeCognomeDisplay = document.getElementById("nomeCognomeDisplay");
+const KmTotaliDisplay = document.getElementById("KmTotaliDisplay");
+const prezzoScontatoDisplay = document.getElementById("prezzoScontatoDisplay");
+const prezzoScontato = document.getElementById("prezzoScontato");
+const valoreScontoDisplay = document.getElementById("valoreScontoDisplay");
+const valoreSconto = document.getElementById("valoreSconto");
+const scontoDisplay = document.getElementById("scontoDisplay");
+const sconto = document.getElementById("sconto");
 
-// Dropdown menu element
 const discountSelect = document.getElementById("discountSelect");
 
-// Calcola il prezzo finale al click del bottone "Genera"
 document.querySelector('input[type="submit"]').addEventListener("click", function(e) {
-  // Prevenire l'invio di form
-  e.preventDefault();
+e.preventDefault();
 
-  // Valore dei km inseriti dall'utente
-  const kmInseriti = document.querySelector('input[type="number"]').value;
+const kmInseriti = document.getElementById("kmInput").value;
+let kmTotali = parseFloat(kmInseriti);
 
-  // Fix per input 0 + prompt per sapere e calcolare km total
-  let kmTotali = parseFloat(kmInseriti);
-  if (isNaN(kmTotali)) {
-    alert("Devi inserire un numero");
-    kmTotali = 0;
-  }
+if (isNaN(kmTotali)) {
+alert("Devi inserire un numero");
+kmTotali = 0;
+}
 
-  // Calcolo il totale in base ai km
-  let prezzoFinale = kmTotali * prezzoAlKm;
-  prezzoFinale = prezzoFinale.toFixed(2);
+const nomeCognomeInseriti = document.getElementById("nomeCognomeInput").value;
+let nomeCognome = (nomeCognomeInseriti);
 
-  // Calcola lo sconto per minori di età o over 65
-  let discount;
-  if (discountSelect && discountSelect.value === "minorenne") {
-    discount = prezzoFinale - (prezzoFinale / 100) * 20;
-  } else if (discountSelect && discountSelect.value === "over65") {
-    discount = prezzoFinale - (prezzoFinale / 100) * 40;
-  } else if (!discountSelect || discountSelect.value === "nessuno") {
-    discount = prezzoFinale;
-  } else
-    discount = prezzoFinale;
+let prezzoFinale = kmTotali * prezzoAlKm;
+prezzoFinale = prezzoFinale.toFixed(2);
 
-  // Mostra il prezzo finale con eventuale sconto
-  prezzoFinaleDisplay.innerHTML = "Prezzo finale: € " + discount;
+KmTotaliDisplay.innerHTML = kmTotali + " Km";
+nomeCognomeDisplay.innerHTML = nomeCognome;
+
+let discount;
+if (discountSelect && discountSelect.value === "minorenne") {
+discount = prezzoFinale - (prezzoFinale / 100) * 20;
+sconto.innerHTML = "Minorenne";
+valoreSconto.innerHTML = "-20%";
+} else if (discountSelect && discountSelect.value === "over65") {
+discount = prezzoFinale - (prezzoFinale / 100) * 40;
+sconto.innerHTML = "Over 65";
+valoreSconto.innerHTML = "-40%";
+} else if (!discountSelect || discountSelect.value === "nessuno") {
+discount = prezzoFinale;
+sconto.innerHTML = "Nessuno";
+valoreSconto.innerHTML = "0%";
+}
+
+prezzoFinaleDisplay.innerHTML = "€" + discount;
 });
