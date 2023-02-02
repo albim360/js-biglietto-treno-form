c1dc2c96ce2493929e3c3b5fa2175ec6cae5aeab
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-02 01:02:20 +0100

    FIX errore input sconto e prezzo totale eff

--- js/app.js
+++ js/app.js
@@ -1,52 +1,47 @@
-const prezzoAlKm = 0.21;
-
-const prezzoFinaleDisplay = document.getElementById("prezzoFinaleDisplay");
+const nomeCognomeInput = document.getElementById("nomeCognomeInput");
+const kmInput = document.getElementById("kmInput");
+const discountSelect = document.getElementById("discountSelect");
+const generaBtn = document.getElementById("generaBtn");
+const annullaBtn = document.getElementById("annullaBtn");
 const nomeCognomeDisplay = document.getElementById("nomeCognomeDisplay");
 const KmTotaliDisplay = document.getElementById("KmTotaliDisplay");
-const prezzoScontatoDisplay = document.getElementById("prezzoScontatoDisplay");
-const prezzoScontato = document.getElementById("prezzoScontato");
-const valoreScontoDisplay = document.getElementById("valoreScontoDisplay");
+const prezzoInteroDisplay = document.getElementById("prezzoInteroDisplay");
+const prezzoFinaleDisplay = document.getElementById("prezzoFinaleDisplay");
 const valoreSconto = document.getElementById("valoreSconto");
 const scontoDisplay = document.getElementById("scontoDisplay");
-const sconto = document.getElementById("sconto");
 
-const discountSelect = document.getElementById("discountSelect");
+generaBtn.addEventListener("click", function () {
+const nomeCognome = nomeCognomeInput.value;
+const km = kmInput.value;
+const sconto = discountSelect.value;
 
-document.querySelector('input[type="submit"]').addEventListener("click", function(e) {
-e.preventDefault();
-
-const kmInseriti = document.getElementById("kmInput").value;
-let kmTotali = parseFloat(kmInseriti);
-
-if (isNaN(kmTotali)) {
-alert("Devi inserire un numero");
-kmTotali = 0;
-}
-
-const nomeCognomeInseriti = document.getElementById("nomeCognomeInput").value;
-let nomeCognome = (nomeCognomeInseriti);
-
-let prezzoFinale = kmTotali * prezzoAlKm;
-prezzoFinale = prezzoFinale.toFixed(2);
-
-KmTotaliDisplay.innerHTML = kmTotali + " Km";
 nomeCognomeDisplay.innerHTML = nomeCognome;
+KmTotaliDisplay.innerHTML = km;
+prezzoInteroDisplay.innerHTML = "€" + (km * 0.21).toFixed(2);
 
-let discount;
-if (discountSelect && discountSelect.value === "minorenne") {
-discount = prezzoFinale - (prezzoFinale / 100) * 20;
-sconto.innerHTML = "Minorenne";
-valoreSconto.innerHTML = "-20%";
-} else if (discountSelect && discountSelect.value === "over65") {
-discount = prezzoFinale - (prezzoFinale / 100) * 40;
-sconto.innerHTML = "Over 65";
-valoreSconto.innerHTML = "-40%";
-} else if (!discountSelect || discountSelect.value === "") {
-discount = prezzoFinale;
-sconto.innerHTML = "Nessuno";
+if (sconto === "minorenne") {
+prezzoFinaleDisplay.innerHTML = "€" + (km * 0.21 * 0.8).toFixed(2);
+valoreSconto.innerHTML = "20%";
+scontoDisplay.innerHTML = "Minorenne";
+} else if (sconto === "over65") {
+prezzoFinaleDisplay.innerHTML = "€" + (km * 0.21 * 0.7).toFixed(2);
+valoreSconto.innerHTML = "30%";
+scontoDisplay.innerHTML = "Over 65";
+} else {
+prezzoFinaleDisplay.innerHTML = "€" + (km * 0.21).toFixed(2);
 valoreSconto.innerHTML = "0%";
+scontoDisplay.innerHTML = "Nessuno";
 }
+});
 
-prezzoFinaleDisplay.innerHTML = "€" + discount;
-discount = discount.toFixed(2);
+annullaBtn.addEventListener("click", function () {
+nomeCognomeInput.value = "";
+kmInput.value = "";
+discountSelect.value = "";
+nomeCognomeDisplay.innerHTML = "";
+KmTotaliDisplay.innerHTML = "";
+prezzoInteroDisplay.innerHTML = "€0.00";
+prezzoFinaleDisplay.innerHTML = "€0.00";
+valoreSconto.innerHTML = "0%";
+scontoDisplay.innerHTML = "Nessuno";
 });
